824e3240aaf90c75d5ea9e2ed894186f3c680ebe
rishavkrm <rishavkumar2573@gmail.com>
2022-08-01 23:40:00 +0530

    currency convertor complete

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import requests
 import tkinter
 import customtkinter
-
-list1 = requests.get("https://v6.exchangerate-api.com/v6/87dce9a4abbc1ef551252f75/latest/USD").json()[
-    "conversion_rates"]
-countries = list(list1.keys())
-
-
-
+from app import App
 app = App()
 app.mainloop()
